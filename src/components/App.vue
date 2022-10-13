be078b08e78906e9abaafb288f78a4a5058a0955
skywolf627 <skywolf627@qq.com>
2022-10-13 16:06:30 +0000

    修复设置窗口标题后，点击窗体标题被还原的问题
    Signed-off-by: skywolf627 <skywolf627@qq.com>

--- src/components/App.vue
+++ src/components/App.vue
@@ -91,7 +91,7 @@ export default {
   },
   watch: {
     app() {
-      this.appData = Object({}, this.app);
+      this.appData = Object({ title: this.appData.title }, this.app);
     },
   },
   data() {
